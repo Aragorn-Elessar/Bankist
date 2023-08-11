f434b39427ab58524d75ecb38560d5ffedded103
Aragorn Elessar <mahmoud_gadallah@outlook.com>
2023-08-11 15:23:22 +0300

    Add date/time to current balance

--- js/script.js
+++ js/script.js
@@ -168,6 +168,16 @@ currentAccount = account1;
 updateUI(currentAccount);
 containerApp.style.opacity = 100;
 
+const now = new Date();
+const day = `${now.getDate()}`.padStart(2, 0);
+const month = `${now.getMonth() + 1}`.padStart(2, 0);
+const year = now.getFullYear();
+const hour = now.getHours();
+const min = `${now.getMinutes()}`.padStart(2, 0);
+labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
+
+// day/month/year
+
 btnLogin.addEventListener('click', function (e) {
   // Prevent form from submitting
   e.preventDefault();
